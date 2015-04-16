485a653fdb96db59c642abdba23741dafe416771
Colin Roth <colin@getwoozy.com>
2015-04-15 20:30:46 -0400

    beginning of tab nabbed

---
+++ Calcohol/MainBLCViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainBLCViewController.h
+//  Calcohol
+//
+//  Created by Colin M. Roth on 4/15/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainBLCViewController : ViewController
+
+@end
---
+++ Calcohol/MainBLCViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainBLCViewController.m
+//  Calcohol
+//
+//  Created by Colin M. Roth on 4/15/15.
+//  Copyright (c) 2015 iamcolinroth. All rights reserved.
+//
+
+#import "MainBLCViewController.h"
+
+@interface MainBLCViewController ()
+
+@end
+
+@implementation MainBLCViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
